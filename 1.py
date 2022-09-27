8d5ab2233661eea472a875ee2096d26cc8a0ff8c
supreme1444 <107640724+supreme1444@users.noreply.github.com>
2022-09-27 13:39:49 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,13 +1,29 @@
-print("Введите кол-во символов и выражение через ENTER")
-a=[]
-n=int(input())
-for i in range(n):
-    a.append(str(input()))
-b='print('
-for i in a:
-    b+=i
+b = '6+6/3'
+d2 = re.split(r'((?:[+-]?\d+(?:\.\d*)?|\.\d+)(?:[eE][-+]?\d+)?)',b)
+d3 = list(filter(None, d2))
+print(d3)
+summa = int('0')
+for i in d3:
+    if '*' in d3:
+        superindex = d3.index('*')
+        left = d3[(superindex - 1)]
+        right = d3[(superindex + 1)]
+        result = int(left) * int(right)
+        del d3[superindex]
+        del d3[superindex]
+        d3[(superindex - 1)] = str(result)
 
-b+=')'
-exec(b)
-  
+    elif '/' in d3:
+        superindex = d3.index('/')
+        left = d3[(superindex - 1)]
+        right = d3[(superindex + 1)]
+        result = int(left) / int(right)
+        del d3[superindex]
+        del d3[superindex]
+        d3[(superindex - 1)] = str(result)
+print (d3)
+for j in d3:
+    summa += float(j)
+
+print('summarizer',summa)
 
