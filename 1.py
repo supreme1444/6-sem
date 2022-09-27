4a64e7fa5fbf981c161725f7831bdba676fbc8b9
supreme1444 <107640724+supreme1444@users.noreply.github.com>
2022-09-27 14:00:01 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,29 +1,28 @@
 b = '6+6/3'
-d2 = re.split(r'((?:[+-]?\d+(?:\.\d*)?|\.\d+)(?:[eE][-+]?\d+)?)',b)
-d3 = list(filter(None, d2))
+lst = re.split(r'((?:[+-]?\d+(?:\.\d*)?|\.\d+)(?:[eE][-+]?\d+)?)',b)
+d3 = list(filter(None, lst))
 print(d3)
 summa = int('0')
 for i in d3:
     if '*' in d3:
-        superindex = d3.index('*')
-        left = d3[(superindex - 1)]
-        right = d3[(superindex + 1)]
+        index = d3.index('*')
+        left = d3[index - 1]
+        right = d3[index + 1]
         result = int(left) * int(right)
-        del d3[superindex]
-        del d3[superindex]
-        d3[(superindex - 1)] = str(result)
+        del d3[index]
+        del d3[index]
+        d3[index - 1] = str(result)
 
     elif '/' in d3:
-        superindex = d3.index('/')
-        left = d3[(superindex - 1)]
-        right = d3[(superindex + 1)]
+        index = d3.index('/')
+        left = d3[index - 1]
+        right = d3[index + 1]
         result = int(left) / int(right)
-        del d3[superindex]
-        del d3[superindex]
-        d3[(superindex - 1)] = str(result)
+        del d3[index]
+        del d3[index]
+        d3[index - 1] = str(result)
 print (d3)
 for j in d3:
     summa += float(j)
 
-print('summarizer',summa)
-
+print(summa)
